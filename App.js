c4f541fd79c4cd876485f489b53004257f410700
Luis Rodríguez <soluciones2000@gmail.com>
2021-08-31 19:14:44 -0400

    Pequeño cambio en notificaciones

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ import Constants from 'expo-constants';
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
   }),
 });
 
